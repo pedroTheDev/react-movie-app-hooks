2ae78de95558521963c63dd9b3cc86ec7ef38605
Pedro Duarte <ikbenpedro@gmail.com>
2021-01-27 14:31:21 -0300

    again

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import Movie from './component/Movie'
 
 const feature_api = "https://api.themoviedb.org/3/movie/popular?api_key=5e14a5761f3f0b7849d62555deb85d02&language=en-US&page=1"
 
-const search_api = "https://api.themoviedb.org/3/search/movie?api_key=5e14a5761f3f0b7849d62555deb85d02&language=en-US&page=1&include_adult=false"
+const search_api = "https://api.themoviedb.org/3/search/movie?api_key=5e14a5761f3f0b7849d62555deb85d02&query="
 
 function App() {
 
   const[ movies, setMovies ] = useState([]);
+  const[ searchTerm, setSearchTerm ] = useState('');
   
   useEffect(() =>{
     fetch(feature_api)
@@ -18,13 +19,48 @@ function App() {
       });
   }, []);
 
+  function handleSubmit(e){
+    e.preventDefault()
+    
+    if(searchTerm){
+    fetch(search_api+searchTerm)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      setMovies(data.results)
+    });
+    setSearchTerm('');
+  }
+
+  }
+  function handleChange(e){
+    setSearchTerm(e.target.value);
+
+  }
+
   return (
+    <>
+    <header className="header">
+      <form onSubmit={handleSubmit}>
+        <input
+        className="search"
+        type="search"
+        placeholder="Search"
+        value={searchTerm}
+        onChange={handleChange}
+        />
+        <button className="chill-btn" type="submit"> ChillFlix </button>
+        </form>
+      </header>
     <div className="movie-container">
+      
       {movies.length > 0 && movies.map((movie) => 
         <Movie key={movie.id} {...movie} />
       )}
       
     </div>
+    </>
   )
-}
+ 
+};
 export default App;
--- src/component/Movie.js
+++ src/component/Movie.js
@@ -2,16 +2,28 @@ import React from 'react'
 
 const img_api = "https://image.tmdb.org/t/p/w500"
 
+const setVoteClass = (vote) => {
+  if(vote >= 8 ){
+    return "green";
+  } else if(vote <= 6){
+    return "red"
+  } else{
+    return "orange"
+  }
+}
+
 const Movie = ({title, poster_path, overview, vote_average}) => {
   return (
     <div className="movie">
     <img src={img_api + poster_path} alt={title} />
       <div className="movie-info">
         <h3>{title}</h3>
-        <span>{vote_average}</span>
+        <span className={`tag ${setVoteClass(vote_average)}`}>{vote_average}</span>
       </div>
       <div className="movie-over">
-        <h2>Overview:</h2>
+        <h2>Overview:
+          <p>{overview}</p>
+        </h2>
       </div>
   </div>
   )
